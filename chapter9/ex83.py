6b1bf7787bd41355b48e82c69abd5efb2ea50465
PhiVanTuan <tuanpv@vega.com.vn>
2020-02-14 11:26:09 +0700

    chap 10 init

--- chapter9/ex83.py
+++ chapter9/ex83.py
@@ -9,13 +9,10 @@ for x in country:
     if ' ' in name: name = name.replace(' ', '_')
     list_country.append(name)
 
-
 def calculate_f_t_c(t, c):
     result=0
-    for x in data:
-        if (t+" "+c) in x or (c+" "+t) in x:result+=1
+    for x in data:result += x.count(t+" "+c)+x.count(c+" "+t)
     return result
-
 
 def calculate_f(t):
     result = 0
@@ -24,9 +21,20 @@ def calculate_f(t):
     return result
 
 def calculate_N(t, c):
-    pass
+    result=0
+    for x in data:
+        result+=x.count(t)+x.count(c)
+    return result
 # print(calculate_f("Viet_Nam"))
 
-for name in list_country:
-    print(name+" : "+str(calculate_f(name)))
+
+# for name in list_country:
+#     print(name+" : "+str(calculate_f(name)))
+def matrix(t):
+    for x in data:
+        if t in x:
+            array_c=x.partition(t)
+
+
+    ft=calculate_f(t)
 
