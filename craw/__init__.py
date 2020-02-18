80f9b393c1a62003e09564a2777b2ebff731b545
PhiVanTuan <tuanpv@vega.com.vn>
2020-02-18 10:54:32 +0700

    craw tiki load all

--- craw/__init__.py
+++ craw/__init__.py
@@ -1,12 +1,24 @@
 import requests
 import json
+import os
 
 url_category = 'https://tiki.vn/api/v2/products?limit=300&sort=position'
 url_product = 'https://tiki.vn/api/v2/reviews?limit=200&sort=score|desc,id|desc,stars|all&include=comments'
+url_all="https://tiki.vn/api/v2/categories?parent_id=2&include_in_menu=true"
 path = '/home/phivantuan/Documents/craw/'
 
+def loadAll():
+    response=requests.get(url_all)
+    str_json=response.json()
+    data=str_json['data']
+    for item in data:
+        category_id=item['id']
+        loadCategory(category_id,1)
 
 def loadCategory(category_id, page):
+    path_=path+str(category_id)
+    if not os.path.exists(path_):
+        os.makedirs(path_)
     params_category = {}
     params_category['category_id'] = category_id
     params_category['page'] = page
@@ -18,14 +30,14 @@ def loadCategory(category_id, page):
             product_id = item['id']
             url = "https://tiki.vn/" + item['url_path']
             lstData = []
-            loadPage(product_id, 1, url, lstData)
+            loadPage(category_id,product_id, 1, url, lstData)
         paging = str_json['paging']
         currentPage = paging['current_page']
         lastPage = paging['last_page']
         if currentPage < lastPage: loadCategory(category_id, currentPage + 1)
 
 
-def loadPage(product_id, page, url, lstData):
+def loadPage(category_id,product_id, page, url, lstData):
     params_product = {}
     params_product['product_id'] = product_id
     params_product['page'] = page
@@ -44,12 +56,12 @@ def loadPage(product_id, page, url, lstData):
         currentPage = page['current_page']
         lastPage = page['last_page']
         if (currentPage < lastPage):
-            loadPage(product_id, currentPage + 1, url, lstData)
+            loadPage(category_id,product_id, currentPage + 1, url, lstData)
         else:
             json_data = json.dumps(lstData)
-            file = open(path + str(product_id) + ".txt", "w")
+            file = open(path + str(category_id)+"/"+str(product_id) + ".txt", "w")
             file.write(json_data)
             file.close()
 
 
-loadCategory(4221, 1)
+loadCategory(1789, 1)
